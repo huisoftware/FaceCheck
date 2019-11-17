3000e6f0319be13161c04ed519c28e01ec7e1176
liuhui <765980018@qq.com>
2019-11-18 00:08:36 +0800

    初始化环境

--- FaceCheck/Form1.Designer.cs
+++ FaceCheck/Form1.Designer.cs
@@ -28,10 +28,17 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.SuspendLayout();
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(800, 450);
-            this.Text = "Form1";
+            this.Name = "Form1";
+            this.Text = "人脸识别签到欢迎系统——北京航空航天大学软件学院";
+            this.ResumeLayout(false);
+
         }
 
         #endregion
