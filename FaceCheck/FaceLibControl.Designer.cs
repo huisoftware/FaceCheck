d0bfab6deed64938232e45942cc1e47f2a89325a
Your Name <you@example.com>
2019-12-21 16:10:26 +0800

    .

--- FaceCheck/FaceLibControl.Designer.cs
+++ FaceCheck/FaceLibControl.Designer.cs
@@ -32,7 +32,7 @@
             this.groupBox1 = new System.Windows.Forms.GroupBox();
             this.dataGridView1 = new System.Windows.Forms.DataGridView();
             this.groupId = new System.Windows.Forms.DataGridViewTextBoxColumn();
-            this.button5 = new System.Windows.Forms.Button();
+            this.stopGroupBt = new System.Windows.Forms.Button();
             this.button4 = new System.Windows.Forms.Button();
             this.button2 = new System.Windows.Forms.Button();
             this.groupBox2 = new System.Windows.Forms.GroupBox();
@@ -54,7 +54,7 @@
             // button1
             // 
             this.button1.Location = new System.Drawing.Point(9, 30);
-            this.button1.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.button1.Margin = new System.Windows.Forms.Padding(4);
             this.button1.Name = "button1";
             this.button1.Size = new System.Drawing.Size(90, 34);
             this.button1.TabIndex = 0;
@@ -65,14 +65,14 @@
             // groupBox1
             // 
             this.groupBox1.Controls.Add(this.dataGridView1);
-            this.groupBox1.Controls.Add(this.button5);
+            this.groupBox1.Controls.Add(this.stopGroupBt);
             this.groupBox1.Controls.Add(this.button4);
             this.groupBox1.Controls.Add(this.button2);
             this.groupBox1.Controls.Add(this.button1);
             this.groupBox1.Location = new System.Drawing.Point(21, 30);
-            this.groupBox1.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.groupBox1.Margin = new System.Windows.Forms.Padding(4);
             this.groupBox1.Name = "groupBox1";
-            this.groupBox1.Padding = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.groupBox1.Padding = new System.Windows.Forms.Padding(4);
             this.groupBox1.Size = new System.Drawing.Size(429, 624);
             this.groupBox1.TabIndex = 1;
             this.groupBox1.TabStop = false;
@@ -84,7 +84,7 @@
             this.dataGridView1.Columns.AddRange(new System.Windows.Forms.DataGridViewColumn[] {
             this.groupId});
             this.dataGridView1.Location = new System.Drawing.Point(9, 74);
-            this.dataGridView1.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.dataGridView1.Margin = new System.Windows.Forms.Padding(4);
             this.dataGridView1.MultiSelect = false;
             this.dataGridView1.Name = "dataGridView1";
             this.dataGridView1.RowHeadersWidth = 62;
@@ -101,20 +101,21 @@
             this.groupId.Name = "groupId";
             this.groupId.Width = 150;
             // 
-            // button5
+            // stopGroupBt
             // 
-            this.button5.Location = new System.Drawing.Point(304, 30);
-            this.button5.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
-            this.button5.Name = "button5";
-            this.button5.Size = new System.Drawing.Size(96, 34);
-            this.button5.TabIndex = 3;
-            this.button5.Text = "停用";
-            this.button5.UseVisualStyleBackColor = true;
+            this.stopGroupBt.Location = new System.Drawing.Point(304, 30);
+            this.stopGroupBt.Margin = new System.Windows.Forms.Padding(4);
+            this.stopGroupBt.Name = "stopGroupBt";
+            this.stopGroupBt.Size = new System.Drawing.Size(96, 34);
+            this.stopGroupBt.TabIndex = 3;
+            this.stopGroupBt.Text = "停用";
+            this.stopGroupBt.UseVisualStyleBackColor = true;
+            this.stopGroupBt.Click += new System.EventHandler(this.stopGroupBt_Click);
             // 
             // button4
             // 
             this.button4.Location = new System.Drawing.Point(208, 30);
-            this.button4.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.button4.Margin = new System.Windows.Forms.Padding(4);
             this.button4.Name = "button4";
             this.button4.Size = new System.Drawing.Size(87, 34);
             this.button4.TabIndex = 2;
@@ -124,7 +125,7 @@
             // button2
             // 
             this.button2.Location = new System.Drawing.Point(108, 30);
-            this.button2.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.button2.Margin = new System.Windows.Forms.Padding(4);
             this.button2.Name = "button2";
             this.button2.Size = new System.Drawing.Size(92, 34);
             this.button2.TabIndex = 1;
@@ -139,9 +140,9 @@
             this.groupBox2.Controls.Add(this.button8);
             this.groupBox2.Controls.Add(this.button7);
             this.groupBox2.Location = new System.Drawing.Point(459, 88);
-            this.groupBox2.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.groupBox2.Margin = new System.Windows.Forms.Padding(4);
             this.groupBox2.Name = "groupBox2";
-            this.groupBox2.Padding = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.groupBox2.Padding = new System.Windows.Forms.Padding(4);
             this.groupBox2.Size = new System.Drawing.Size(614, 566);
             this.groupBox2.TabIndex = 2;
             this.groupBox2.TabStop = false;
@@ -155,7 +156,7 @@
             this.userName,
             this.isSign});
             this.dataGridView2.Location = new System.Drawing.Point(12, 74);
-            this.dataGridView2.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.dataGridView2.Margin = new System.Windows.Forms.Padding(4);
             this.dataGridView2.MultiSelect = false;
             this.dataGridView2.Name = "dataGridView2";
             this.dataGridView2.RowHeadersWidth = 62;
@@ -188,7 +189,7 @@
             // button9
             // 
             this.button9.Location = new System.Drawing.Point(260, 30);
-            this.button9.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.button9.Margin = new System.Windows.Forms.Padding(4);
             this.button9.Name = "button9";
             this.button9.Size = new System.Drawing.Size(112, 34);
             this.button9.TabIndex = 5;
@@ -198,7 +199,7 @@
             // button8
             // 
             this.button8.Location = new System.Drawing.Point(138, 30);
-            this.button8.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.button8.Margin = new System.Windows.Forms.Padding(4);
             this.button8.Name = "button8";
             this.button8.Size = new System.Drawing.Size(112, 34);
             this.button8.TabIndex = 4;
@@ -208,7 +209,7 @@
             // button7
             // 
             this.button7.Location = new System.Drawing.Point(16, 30);
-            this.button7.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.button7.Margin = new System.Windows.Forms.Padding(4);
             this.button7.Name = "button7";
             this.button7.Size = new System.Drawing.Size(112, 34);
             this.button7.TabIndex = 3;
@@ -218,7 +219,7 @@
             // button3
             // 
             this.button3.Location = new System.Drawing.Point(878, 45);
-            this.button3.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.button3.Margin = new System.Windows.Forms.Padding(4);
             this.button3.Name = "button3";
             this.button3.Size = new System.Drawing.Size(195, 34);
             this.button3.TabIndex = 2;
@@ -244,7 +245,7 @@
             this.Controls.Add(this.groupBox2);
             this.Controls.Add(this.groupBox1);
             this.Controls.Add(this.button3);
-            this.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.Margin = new System.Windows.Forms.Padding(4);
             this.Name = "FaceLibControl";
             this.Size = new System.Drawing.Size(1095, 675);
             this.Load += new System.EventHandler(this.FaceLibControl_Load);
@@ -261,7 +262,7 @@
 
         private System.Windows.Forms.Button button1;
         private System.Windows.Forms.GroupBox groupBox1;
-        private System.Windows.Forms.Button button5;
+        private System.Windows.Forms.Button stopGroupBt;
         private System.Windows.Forms.Button button4;
         private System.Windows.Forms.Button button2;
         private System.Windows.Forms.GroupBox groupBox2;
