a2a03b5adcdaee1fbd81d8eddd980179b06c3481
liuhui <765980018@qq.com>
2019-12-21 15:27:07 +0800

    解决冲突

--- FaceCheck/FaceLibControl.cs
+++ FaceCheck/FaceLibControl.cs
@@ -9,6 +9,7 @@ using System.Threading.Tasks;
 using System.Windows.Forms;
 using Baidu.Aip.Speech;
 using Baidu.Aip.Face;
+using System.IO;
 
 namespace FaceCheck
 {
@@ -20,7 +21,6 @@ namespace FaceCheck
         public FaceLibControl(Face clientAll, Tts client2All)
         {
             InitializeComponent();
-
             Face client = clientAll;
             Tts client2 = client2All;
             form = this;
@@ -77,6 +77,127 @@ namespace FaceCheck
         {
             groupList.Remove("需要删除");
             renovate(groupList);
+        }
+
+        private void button7_Click(object sender, EventArgs e)
+        {
+            if (dataGridView1.SelectedRows.Count < 1)
+            {
+                MessageBox.Show("请先选择用户组", "错误");
+                return;
+            }
+            OpenFileDialog dialog = new OpenFileDialog();
+            dialog.Multiselect = true;//该值确定是否可以选择多个文件
+            dialog.Title = "请选择图片";
+            dialog.Filter = "图片文件(*.png;*.jpg;*.bmp)|*.png;*.jpg;*.bmp";
+            if (dialog.ShowDialog() == DialogResult.OK)
+            {
+                string[] files = dialog.FileNames;
+                var groupID = dataGridView1.CurrentCell.Value.ToString();
+                string userName = string.Empty;
+                InputName.Show(out userName);
+                register(files, groupID, userName);
+            }
+            
+        }
+
+        
+
+        private string register(string[] imgPaths, string groupID, string name)
+        {
+            var uid = GenerateStringID();
+            foreach (string img in imgPaths)
+            {
+                var image = readImg(img);
+
+                var imageType = "BASE64";
+
+                var groupId = groupID;
+
+                var userId = uid;
+
+                // 如果有可选参数
+                var options = new Dictionary<string, object>{
+                    {"user_info", name},
+                };
+                // 带参数调用人脸注册
+                var result = client.UserAdd(image, imageType, groupId, userId, options);
+            }
+            return uid;
+        }
+
+        private string readImg(string img)
+        {
+            return Convert.ToBase64String(File.ReadAllBytes(img));
+        }
+
+        private string GenerateStringID()
+
+        {
+
+            long i = 1;
+
+            foreach (byte b in Guid.NewGuid().ToByteArray())
+
+            {
+
+                i *= ((int)b + 1);
+
+            }
+
+            return string.Format("{0:x}", i - DateTime.Now.Ticks);
+
+        }
+
+        private void updateFaceBt_Click(object sender, EventArgs e)
+        {
+            if (userList.SelectedRows.Count < 1)
+            {
+                MessageBox.Show("请先选择用户", "错误");
+                return;
+            }
+
+            OpenFileDialog dialog = new OpenFileDialog();
+            dialog.Multiselect = true;//该值确定是否可以选择多个文件
+            dialog.Title = "请选择图片";
+            dialog.Filter = "图片文件(*.png;*.jpg;*.bmp)|*.png;*.jpg;*.bmp";
+            if (dialog.ShowDialog() == DialogResult.OK)
+            {
+                string[] files = dialog.FileNames;
+                var groupID = dataGridView1.CurrentCell.Value.ToString();
+                var uid = userList.CurrentCell.Value.ToString();
+                string userName = string.Empty;
+                InputName.Show(out userName);
+                update(files, groupID, userName, uid);
+            }
+        }
+
+        private string update(string[] imgPaths, string groupID, string name, string uid)
+        {
+            var key = 1;
+            foreach (string img in imgPaths)
+            {
+                var image = readImg(img);
+
+                var imageType = "BASE64";
+
+                var groupId = groupID;
+
+                var userId = uid;
+
+                // 如果有可选参数
+                var options = new Dictionary<string, object>{
+                    {"user_info", name},
+                };
+                if (key == 1)
+                {
+                    options.Add("action_type", "REPLACE ");
+                }
+                // 带参数调用人脸注册
+                var result = client.UserUpdate(image, imageType, groupId, userId, options);
+                key++;
+            }
+            return uid;
         }
     }
 }
